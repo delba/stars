fee04445680c3137d527d15dfaa886aaf93fc0cc
delba <delba@users.noreply.github.com>
2015-03-05 15:22:51 -0500

    Extract methods in github

--- github/github.go
+++ github/github.go
@@ -1,78 +1,141 @@
 package github
 
-import "github.com/octokit/go-octokit/octokit"
+import (
+	"net/http"
 
-var client = octokit.NewClient(nil)
+	"github.com/delba/stars/model"
+	"github.com/octokit/go-octokit/octokit"
+	"golang.org/x/oauth2"
+)
 
-func GetFollowingStarred(username string) ([]octokit.Repository, error) {
-	c := make(chan []octokit.Repository)
-	var repositories []octokit.Repository
+const (
+	ClientID     = "aa78f0f464d4a73010a0"
+	ClientSecret = "842311922d9dd09ee074f63cf0218f9db2c75056"
 
-	user, err := GetUser(username)
+	AuthURL     = "https://github.com/login/oauth/authorize"
+	TokenURL    = "https://github.com/login/oauth/access_token"
+	RedirectURL = "http://localhost:8080/callback"
+)
+
+var config = oauth2.Config{
+	ClientID:     ClientID,
+	ClientSecret: ClientSecret,
+	Endpoint: oauth2.Endpoint{
+		AuthURL:  AuthURL,
+		TokenURL: TokenURL,
+	},
+	RedirectURL: RedirectURL,
+}
+
+var Client *octokit.Client
+var CurrentUser *octokit.User
+
+func GetAuthURL() string {
+	return config.AuthCodeURL(randomString())
+}
+
+func GetClient(r *http.Request) (*octokit.Client, error) {
+	var client *octokit.Client
+	var err error
+	code := r.URL.Query()["code"][0]
+	token, err := config.Exchange(nil, code)
 	if err != nil {
-		return repositories, err
+		return client, err
 	}
 
-	following, err := GetFollowing(user)
+	client = octokit.NewClient(octokit.TokenAuth{token.AccessToken})
+
+	return client, err
+}
+
+func GetCurrentUser() (*octokit.User, error) {
+	var user *octokit.User
+	var err error
+
+	url, err := octokit.CurrentUserURL.Expand(nil)
 	if err != nil {
-		return repositories, err
+		return user, err
+	}
+
+	user, result := Client.Users(url).One()
+	if result.HasError() {
+		return user, err
+	}
+
+	return user, err
+}
+
+func GetFollowing() ([]octokit.User, error) {
+	var following []octokit.User
+
+	followingURL, err := CurrentUser.FollowingURL.Expand(nil)
+	if err != nil {
+		return following, err
+	}
+
+	users, result := Client.Users(followingURL).All()
+	if result.HasError() {
+		return following, err
+	}
+	following = append(following, users...)
+
+	// TODO goroutine
+	for {
+		if result.NextPage == nil {
+			break
+		}
+		nextPageURL, _ := result.NextPage.Expand(nil)
+		users, result = Client.Users(nextPageURL).All()
+		if result.HasError() {
+			break
+		}
+		following = append(following, users...)
+	}
+
+	return following, err
+}
+
+func GetFollowingStarred() (model.StarredRepositories, error) {
+	var starredRepositories model.StarredRepositories
+	var err error
+
+	c := make(chan map[octokit.User][]octokit.Repository)
+
+	following, err := GetFollowing()
+	if err != nil {
+		return starredRepositories, err
 	}
 
 	for _, user := range following {
-		go func(u octokit.User, c chan []octokit.Repository) {
-			starred, err := GetStarred(user)
-			if err != nil {
-				panic(err)
-			}
-			c <- starred
-		}(user, c)
+		go GetStarredRepositories(user, c)
 	}
 
 	for range following {
-		repositories = append(repositories, <-c...)
+		for user, repos := range <-c {
+			for _, repo := range repos {
+				starredRepository := starredRepositories.FindOrCreateByRepository(repo)
+				starredRepository.Users = append(starredRepository.Users, &user)
+			}
+		}
 	}
 
-	return repositories, err
+	return starredRepositories, err
 }
 
-func GetUser(username string) (*octokit.User, error) {
-	url, err := octokit.UserURL.Expand(octokit.M{"user": username})
+func GetStarredRepositories(u octokit.User, c chan map[octokit.User][]octokit.Repository) {
+	url, err := u.StarredURL.Expand(nil)
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
 
-	user, result := client.Users(url).One()
+	repositories, result := Client.Repositories(url).All()
 	if result.HasError() {
-		return nil, result.Err
+		panic(result.Err)
 	}
 
-	return user, nil
+	c <- map[octokit.User][]octokit.Repository{u: repositories}
 }
 
-func GetFollowing(user *octokit.User) ([]octokit.User, error) {
-	url, err := user.FollowingURL.Expand(nil)
-	if err != nil {
-		return nil, err
-	}
-
-	following, result := client.Users(url).All()
-	if result.HasError() {
-		return nil, result.Err
-	}
-
-	return following, nil
-}
-
-func GetStarred(user octokit.User) ([]octokit.Repository, error) {
-	url, err := user.StarredURL.Expand(nil)
-	if err != nil {
-		return nil, err
-	}
-
-	starred, result := client.Repositories(url).All()
-	if result.HasError() {
-		return nil, result.Err
-	}
-
-	return starred, nil
+func randomString() string {
+	return "hello"
 }
--- main.go
+++ main.go
@@ -8,32 +8,9 @@ import (
 	"sort"
 	"text/template"
 
+	"github.com/delba/stars/github"
 	"github.com/delba/stars/model"
-	"github.com/octokit/go-octokit/octokit"
-	"golang.org/x/oauth2"
 )
-
-const (
-	ClientID     = "aa78f0f464d4a73010a0"
-	ClientSecret = "842311922d9dd09ee074f63cf0218f9db2c75056"
-
-	AuthURL     = "https://github.com/login/oauth/authorize"
-	TokenURL    = "https://github.com/login/oauth/access_token"
-	RedirectURL = "http://localhost:8080/callback"
-)
-
-var config = oauth2.Config{
-	ClientID:     ClientID,
-	ClientSecret: ClientSecret,
-	Endpoint: oauth2.Endpoint{
-		AuthURL:  AuthURL,
-		TokenURL: TokenURL,
-	},
-	RedirectURL: RedirectURL,
-}
-
-var client *octokit.Client
-var currentUser *octokit.User
 
 func handle(err error) {
 	if err != nil {
@@ -56,7 +33,7 @@ func main() {
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	if currentUser == nil {
+	if github.CurrentUser == nil {
 		PublicIndex(w, r)
 	} else {
 		PrivateIndex(w, r)
@@ -76,7 +53,7 @@ func PublicIndex(w http.ResponseWriter, r *http.Request) {
 func PrivateIndex(w http.ResponseWriter, r *http.Request) {
 	var err error
 
-	starredRepositories, err := GetFollowingStarred()
+	starredRepositories, err := github.GetFollowingStarred()
 	sort.Sort(model.ByPopularity(starredRepositories))
 
 	_, filename, _, _ := runtime.Caller(1)
@@ -89,7 +66,7 @@ func PrivateIndex(w http.ResponseWriter, r *http.Request) {
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
-	url := config.AuthCodeURL(randomString())
+	url := github.GetAuthURL()
 
 	http.Redirect(w, r, url, 302)
 }
@@ -97,122 +74,16 @@ func Login(w http.ResponseWriter, r *http.Request) {
 func Callback(w http.ResponseWriter, r *http.Request) {
 	var err error
 
-	client, err = GetClient(r)
+	github.Client, err = github.GetClient(r)
 	handle(err)
 
-	currentUser, err = GetCurrentUser()
+	github.CurrentUser, err = github.GetCurrentUser()
 	handle(err)
 
 	http.Redirect(w, r, "/", 302)
-}
-
-func GetClient(r *http.Request) (*octokit.Client, error) {
-	var client *octokit.Client
-	var err error
-	code := r.URL.Query()["code"][0]
-	token, err := config.Exchange(nil, code)
-	if err != nil {
-		return client, err
-	}
-
-	client = octokit.NewClient(octokit.TokenAuth{token.AccessToken})
-
-	return client, err
-}
-
-func GetCurrentUser() (*octokit.User, error) {
-	var user *octokit.User
-	var err error
-
-	url, err := octokit.CurrentUserURL.Expand(nil)
-	if err != nil {
-		return user, err
-	}
-
-	user, result := client.Users(url).One()
-	if result.HasError() {
-		return user, err
-	}
-
-	return user, err
 }
 
 func Logout(w http.ResponseWriter, r *http.Request) {
-	client, currentUser = nil, nil
+	github.Client, github.CurrentUser = nil, nil
 	http.Redirect(w, r, "/", 302)
-}
-
-func GetFollowing() ([]octokit.User, error) {
-	var following []octokit.User
-
-	followingURL, err := currentUser.FollowingURL.Expand(nil)
-	if err != nil {
-		return following, err
-	}
-
-	users, result := client.Users(followingURL).All()
-	if result.HasError() {
-		return following, err
-	}
-	following = append(following, users...)
-
-	// TODO goroutine
-	for {
-		if result.NextPage == nil {
-			break
-		}
-		nextPageURL, _ := result.NextPage.Expand(nil)
-		users, result = client.Users(nextPageURL).All()
-		if result.HasError() {
-			break
-		}
-		following = append(following, users...)
-	}
-
-	return following, err
-}
-
-func GetFollowingStarred() (model.StarredRepositories, error) {
-	var starredRepositories model.StarredRepositories
-	var err error
-
-	c := make(chan map[octokit.User][]octokit.Repository)
-
-	following, err := GetFollowing()
-	if err != nil {
-		return starredRepositories, err
-	}
-
-	for _, user := range following {
-		go GetStarredRepositories(user, c)
-	}
-
-	for range following {
-		for user, repos := range <-c {
-			for _, repo := range repos {
-				starredRepository := starredRepositories.FindOrCreateByRepository(repo)
-				starredRepository.Users = append(starredRepository.Users, &user)
-			}
-		}
-	}
-
-	return starredRepositories, err
-}
-
-func GetStarredRepositories(u octokit.User, c chan map[octokit.User][]octokit.Repository) {
-	url, err := u.StarredURL.Expand(nil)
-	if err != nil {
-		panic(err)
-	}
-
-	repositories, result := client.Repositories(url).All()
-	if result.HasError() {
-		panic(result.Err)
-	}
-
-	c <- map[octokit.User][]octokit.Repository{u: repositories}
-}
-
-func randomString() string {
-	return "hello"
 }
